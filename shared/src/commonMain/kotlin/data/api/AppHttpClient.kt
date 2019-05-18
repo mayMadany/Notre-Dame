62d3204b53e1765492175c8c8c744b88e74f7367
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2019-05-18 11:22:06 -0400

    Add HttpCookies to client

--- shared/src/commonMain/kotlin/data/api/AppHttpClient.kt
+++ shared/src/commonMain/kotlin/data/api/AppHttpClient.kt
@@ -1,6 +1,8 @@
 package data.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.cookies.AcceptAllCookiesStorage
+import io.ktor.client.features.cookies.HttpCookies
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.defaultSerializer
 
@@ -13,6 +15,11 @@ object AppHttpClient {
         HttpClient {
             install(JsonFeature) {
                 serializer = defaultSerializer()
+            }
+
+            install(HttpCookies) {
+                // Will keep an in-memory map with all the cookies from previous requests.
+                storage = AcceptAllCookiesStorage()
             }
         }
     }
